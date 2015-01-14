5b2a69f20f420c1f16a2ec3107bcd44d68ede7d5
Isaac Assegai <isaac.a.travers@gmail.com>
2015-01-13 19:14:32 -0800

    working on spritev2

--- src/spacegame/graphics/SpriteV2.java
+++ src/spacegame/graphics/SpriteV2.java
@@ -2,11 +2,99 @@ package spacegame.graphics;
 import spacegame.tilegame.ResourceManager;
 import spacegame.util.Vector2D;
 
-public class SpriteV2 {
+public abstract class SpriteV2 {
 	protected ResourceManager parent;
+	protected Vector2D currentForce;
 	protected Vector2D velocity;
     protected Vector2D position;
     protected Vector2D heading;
-    public Vector2D side;
+    protected Vector2D side;
+    protected double mass;
+    protected double maxSpeed;
+    protected double maxTurnRate;
+    
+    /** Creates a new Sprite object with the specified Animation. */
+	public SpriteV2(ResourceManager parent) {
+		 this.parent = parent;
+		 currentForce = new Vector2D(0, 0);
+	     velocity = new Vector2D(0, 0);
+	     position = new Vector2D(0, 0);
+	     heading = new Vector2D(1, 0);
+	     side = new Vector2D(0,1);
+	}
+	
+	public ResourceManager getParent() {
+		return parent;
+	}
+
+	public void setParent(ResourceManager parent) {
+		this.parent = parent;
+	}
+
+	public Vector2D getCurrentForce() {
+		return currentForce;
+	}
+
+	public void setCurrentForce(Vector2D currentForce) {
+		this.currentForce = currentForce;
+	}
+
+	public Vector2D getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector2D velocity) {
+		this.velocity = velocity;
+	}
+
+	public Vector2D getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2D position) {
+		this.position = position;
+	}
+
+	public Vector2D getHeading() {
+		return heading;
+	}
+
+	public void setHeading(Vector2D heading) {
+		this.heading = heading;
+	}
+
+	public Vector2D getSide() {
+		return side;
+	}
+
+	public void setSide(Vector2D side) {
+		this.side = side;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public double getMaxTurnRate() {
+		return maxTurnRate;
+	}
+
+	public void setMaxTurnRate(double maxTurnRate) {
+		this.maxTurnRate = maxTurnRate;
+	}
+
+	public abstract void update(double elapsedTime);
 	
 }
